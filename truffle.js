cefd8c9b9e6546de8dd96b418b99d3746f7a445e
Aler Denisov <aler.zampillo@gmail.com>
2018-03-06 11:30:50 +0300

    custom provider for rinkeby network

--- truffle.js
+++ truffle.js
@@ -1,18 +1,12 @@
 require('babel-polyfill')
 require('babel-register')({
   // Ignore everything in node_modules except node_modules/zeppelin-solidity. 
-  presets: ['env'],
+  presets: ['es2015'],
   plugins: ['syntax-async-functions','transform-regenerator'],
   ignore: /node_modules\/(?!zeppelin-solidity)/, 
 })
 
-let provider
 const HDWalletProvider = require('truffle-hdwallet-provider-privkey')
-const privateKey = process.env.PRIVATE_KEY
-
-if (!process.env.SOLIDITY_COVERAGE && privateKey) {
-  provider = new HDWalletProvider(privateKey, 'https://rinkeby.infura.io')
-}
 
 module.exports = {
   networks: {
@@ -25,7 +19,7 @@ module.exports = {
       from: '0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1'     // Use the address we derived
     },
     rinkeby: {
-      provider: provider,
+      provider: () => new HDWalletProvider(process.env.PRIVATE_KEY, 'https://rinkeby.infura.io'),
       network_id: 4,
       gas: 4.5 * 1e6,
       gasPrice: 5e9
